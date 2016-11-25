801d9334dd03f9bde46d7df908c0b67d803d87e7
“your_usCCCCCCCCCCCCCtongtao.zhu <tongtao.zhu@dianping.com>
2016-11-25 14:18:10 +0800

    init project

---
+++ Class1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Windows.Forms;
+using Fiddler;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+public class Violin : IAutoTamper    // Ensure class is public, or Fiddler won't see it!
+{
+    private UserInterface myCtrl; //MyControl自定义控件
+
+    public void OnLoad()
+    {
+        /* Load your UI here */
+        myCtrl = new UserInterface();
+    }
+    public void OnBeforeUnload() { }
+
+    public void AutoTamperRequestBefore(Session oSession)
+    {
+        
+    }
+    public void AutoTamperRequestAfter(Session oSession) {
+        if (oSession.url.Contains("mp.weixin.qq.com/mp/getmasssendmsg?__biz")) {
+            FileStream fs = new FileStream("D:/cookie.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter sw = new StreamWriter(fs);
+            string cookie = oSession.RequestHeaders.AllValues("Cookie");
+            string url = oSession.url;
+            string pattern = @"frommsgid=\d{5,}";
+            MatchCollection matchs = Regex.Matches(url, pattern);
+            string frommsgid = "";
+            foreach (Match match in matchs) {
+                frommsgid = match.Value;
+                myCtrl.AddResult(frommsgid);
+            }
+            sw.WriteLine(cookie+"\t"+ url+ "\t"+ frommsgid);
+            sw.Close();
+        }
+    }
+    public void AutoTamperResponseBefore(Session oSession) { }
+    public void AutoTamperResponseAfter(Session oSession) { }
+    public void OnBeforeReturningError(Session oSession) { }
+}
---
+++ UserInterface.cs
@@ -0,0 +1,83 @@
+﻿using Fiddler;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+class UserInterface : UserControl
+{
+    private TabPage tabPage; //添加一个标签页 用来放置控件
+    private CheckBox chkb_Enabled;  //用来启用或禁用插件
+    private TextBox textBox_Result;  //用来保存最后的结果
+    private Button btn_Clear;  //清空按钮
+    public bool bEnabled;
+    public delegate void Delegate_AddResult(string strUrl);//定义输出结果的委托
+    public UserInterface()
+    {
+        this.bEnabled = false;
+        this.InitializeUI();
+        FiddlerApplication.UI.tabsViews.TabPages.Add(this.tabPage); //新建一个tabPage
+    }
+
+    public void InitializeUI() //初始化UI
+    {
+        this.tabPage = new TabPage("WechatArticle");
+        this.tabPage.AutoScroll = true;
+
+        this.chkb_Enabled = new CheckBox();
+        this.chkb_Enabled.Top = 10;
+        this.chkb_Enabled.Left = 20;
+        this.chkb_Enabled.Text = "Enable";
+        this.chkb_Enabled.Checked = false;  //初始化为不选中
+
+        this.btn_Clear = new Button();
+        this.btn_Clear.Text = "Clear";
+        this.btn_Clear.Left = 200;
+        this.btn_Clear.Top = 10;
+        this.Enabled = false;
+
+        this.textBox_Result = new TextBox();
+        this.textBox_Result.Top = 50;
+        this.textBox_Result.Left = 20;
+        this.textBox_Result.Width = 1000;
+        this.textBox_Result.Height = 600;
+        this.textBox_Result.ReadOnly = true;
+        this.textBox_Result.Multiline = true;  //多行显示
+        this.textBox_Result.ScrollBars = ScrollBars.Vertical;  //垂直滚动条
+
+        this.tabPage.Controls.Add(this.chkb_Enabled);
+        this.tabPage.Controls.Add(this.btn_Clear);
+        this.tabPage.Controls.Add(this.textBox_Result);
+
+        /*给chkb_Enabled添加CheckedChanged事件处理*/
+        this.chkb_Enabled.CheckedChanged += new EventHandler(this.chkb_Enabled_CheckedChanged);
+        this.btn_Clear.Click += new EventHandler(this.btn_Clear_Click);
+    }
+
+    private void btn_Clear_Click(object obj, EventArgs args)
+    {
+        this.textBox_Result.Text = "";
+    }
+    private void chkb_Enabled_CheckedChanged(object obj, EventArgs args)
+    {
+        this.SuspendLayout();
+        this.bEnabled = this.chkb_Enabled.Checked;
+        this.btn_Clear.Enabled = this.bEnabled;
+        this.ResumeLayout();
+    }
+
+    public void AddResult(string strUrl)
+    {
+        if (!this.textBox_Result.InvokeRequired)
+            this.textBox_Result.AppendText(strUrl + "\r\n");
+        else
+        {
+            Delegate_AddResult delegate_addresult = new Delegate_AddResult(this.AddResult);
+            this.textBox_Result.Invoke(delegate_addresult, strUrl);
+        }
+    }
+
+}
